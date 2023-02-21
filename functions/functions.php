38b63eb31488785522dfb41649baace11fb689b7
romamaslov200 <romanmaslov200@gmail.com>
2023-02-21 17:25:38 +0300

    Сделал Фукции

---
+++ functions/functions.php
@@ -0,0 +1,14 @@
+<?php
+
+function menu()
+{
+    ?>
+    <ul class="menu">
+        <li><a class="menu_text" href="">Главная</a></li>
+        <li><a class="menu_text" href="">Главная</a></li>
+        <li><a class="menu_text" href="">Главная</a></li>
+    </ul>
+    <?php
+}
+
+?>
