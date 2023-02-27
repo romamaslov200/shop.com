b02270f8d8ab5dec745121a045ee57b4a4ed1d77
romamaslov200 <romanmaslov200@gmail.com>
2023-02-27 21:13:56 +0300

    2

--- index.php
+++ index.php
@@ -14,71 +14,74 @@ include "include/functions.php";
     <?php
     menu();
     ?>
-    <div class="list_product">
-        <div class="product">
-            <img class="img_product" src="https://www.pngarts.com/files/4/Dress-Shirt-PNG-Transparent-Image.png" alt="">
-            <div>
-                <p class="title_product">Рубашка муржска</p>
-                <p class="text_product">Стильная муржская рубашка</p>
+    <div class="wrapper">
+        <div class="list_product">
+            <div class="product">
+                <img class="img_product" src="https://ae01.alicdn.com/kf/HTB16E9XSVXXXXaeaXXXq6xXFXXXu/2017-s.jpg" alt="">
+                <div>
+                    <p class="title_product">Рубашка муржска</p>
+                    <p class="text_product">Стильная муржская рубашка</p>
+                    <a href=""></a>
+                </div>
             </div>
-        </div>
-        <div class="product">
-            <img class="img_product" src="https://www.pngarts.com/files/4/Dress-Shirt-PNG-Transparent-Image.png" alt="">
-            <div>
-                <p class="title_product">Рубашка муржска</p>
-                <p class="text_product">Стильная муржская рубашка</p>
+            <div class="product">
+                <img class="img_product" src="https://i4.stat01.com/1/7425/74240100/afacdb/wrangler-ssha.jpg" alt="">
+                <div>
+                    <p class="title_product">Рубашка муржска</p>
+                    <p class="text_product">Стильная муржская рубашка</p>
+                </div>
             </div>
-        </div>
-        <div class="product">
-            <img class="img_product" src="https://www.pngarts.com/files/4/Dress-Shirt-PNG-Transparent-Image.png" alt="">
-            <div>
-                <p class="title_product">Рубашка муржска</p>
-                <p class="text_product">Стильная муржская рубашка</p>
+            <div class="product">
+                <img class="img_product" src="https://www.pngarts.com/files/4/Dress-Shirt-PNG-Transparent-Image.png" alt="">
+                <div>
+                    <p class="title_product">Рубашка муржска</p>
+                    <p class="text_product">Стильная муржская рубашка</p>
+                </div>
             </div>
-        </div>
-        <div class="product">
-            <img class="img_product" src="https://www.pngarts.com/files/4/Dress-Shirt-PNG-Transparent-Image.png" alt="">
-            <div>
-                <p class="title_product">Рубашка муржска</p>
-                <p class="text_product">Стильная муржская рубашка</p>
+            <div class="product">
+                <img class="img_product" src="https://www.pngarts.com/files/4/Dress-Shirt-PNG-Transparent-Image.png" alt="">
+                <div>
+                    <p class="title_product">Рубашка муржска</p>
+                    <p class="text_product">Стильная муржская рубашка</p>
+                </div>
             </div>
-        </div>
-        <div class="product">
-            <img class="img_product" src="https://www.pngarts.com/files/4/Dress-Shirt-PNG-Transparent-Image.png" alt="">
-            <div>
-                <p class="title_product">Рубашка муржска</p>
-                <p class="text_product">Стильная муржская рубашка</p>
+            <div class="product">
+                <img class="img_product" src="https://www.pngarts.com/files/4/Dress-Shirt-PNG-Transparent-Image.png" alt="">
+                <div>
+                    <p class="title_product">Рубашка муржска</p>
+                    <p class="text_product">Стильная муржская рубашка</p>
+                </div>
             </div>
-        </div>
 
-        <div class="product">
-            <img class="img_product" src="https://www.pngarts.com/files/4/Dress-Shirt-PNG-Transparent-Image.png" alt="">
-            <div>
-                <p class="title_product">Рубашка муржска</p>
-                <p class="text_product">Стильная муржская рубашка</p>
+            <div class="product">
+                <img class="img_product" src="https://www.pngarts.com/files/4/Dress-Shirt-PNG-Transparent-Image.png" alt="">
+                <div>
+                    <p class="title_product">Рубашка муржска</p>
+                    <p class="text_product">Стильная муржская рубашка</p>
+                </div>
+            </div>
+            <div class="product">
+                <img class="img_product" src="https://www.pngarts.com/files/4/Dress-Shirt-PNG-Transparent-Image.png" alt="">
+                <div>
+                    <p class="title_product">Рубашка муржска</p>
+                    <p class="text_product">Стильная муржская рубашка</p>
+                </div>
+            </div>
+            <div class="product">
+                <img class="img_product" src="https://www.pngarts.com/files/4/Dress-Shirt-PNG-Transparent-Image.png" alt="">
+                <div>
+                    <p class="title_product">Рубашка муржска</p>
+                    <p class="text_product">Стильная муржская рубашка</p>
+                </div>
+            </div>
+            <div class="product">
+                <img class="img_product" src="https://www.pngarts.com/files/4/Dress-Shirt-PNG-Transparent-Image.png" alt="">
+                <div>
+                    <p class="title_product">Рубашка муржска</p>
+                    <p class="text_product">Стильная муржская рубашка</p>
+                </div>
             </div>
         </div>
-        <div class="product">
-            <img class="img_product" src="https://www.pngarts.com/files/4/Dress-Shirt-PNG-Transparent-Image.png" alt="">
-            <div>
-                <p class="title_product">Рубашка муржска</p>
-                <p class="text_product">Стильная муржская рубашка</p>
-            </div>
-        </div>
-        <div class="product">
-            <img class="img_product" src="https://www.pngarts.com/files/4/Dress-Shirt-PNG-Transparent-Image.png" alt="">
-            <div>
-                <p class="title_product">Рубашка муржска</p>
-                <p class="text_product">Стильная муржская рубашка</p>
-            </div>
-        </div>
-        <div class="product">
-            <img class="img_product" src="https://www.pngarts.com/files/4/Dress-Shirt-PNG-Transparent-Image.png" alt="">
-            <div>
-                <p class="title_product">Рубашка муржска</p>
-                <p class="text_product">Стильная муржская рубашка</p>
-            </div>
-        </div>
-        
+    </div>        
 </body>
 </html>
---
+++ product.php
@@ -0,0 +1,32 @@
+<?php
+include "include/functions.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css?<?= time()?>">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    menu();
+    ?>
+    <div class="wrapper">
+        <div class="container-viv">
+            <div class="grid-center">
+                <div class="product-viv">
+                    <img class="img_product-viv" src="https://www.pngarts.com/files/4/Dress-Shirt-PNG-Transparent-Image.png" alt="">
+                    <div>
+                        <p class="title_product">Рубашка муржска</p>
+                        <p class="text_product">Стильная муржская рубашкаСтильная муржская рубашкаСтильная муржская рубашкаСтильная муржская рубашкаСтильная муржская рубашкаСтильная муржская рубашкаСтильная муржская рубашкаСтильная муржская рубашкаСтильная муржская рубашкаСтильная муржская рубашкаСтильная муржская рубашкаСтильная муржская рубашкаСтильная муржская рубашкаСтильная муржская рубашкаСтильная муржская рубашкаСтильная муржская рубашкаСтильная муржская рубашкаСтильная муржская рубашкаСтильная муржская рубашкаСтильная муржская рубашкаСтильная муржская рубашкаСтильная муржская рубашкаСтильная муржская рубашкаСтильная муржская рубашкаСтильная муржская рубашка</p>
+                    </div>
+                    <button class="btn1">Купить</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
