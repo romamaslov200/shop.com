749d992b9c1d5d5ae2d3c900618e653383cc8170
romamaslov200 <romanmaslov200@gmail.com>
2023-02-27 21:39:35 +0300

    +link -php

--- include/functions.php
+++ include/functions.php
@@ -5,7 +5,7 @@ function menu()
 {
     ?>
     <ul class="menu">
-        <li class="menu_li"><a class="menu_text" href="">Главная</a></li>
+        <li class="menu_li"><a class="menu_text" href="index">Главная</a></li>
         <li class="menu_li"><a class="menu_text" href="">Поиск</a></li>
         <li class="menu_li"><a class="menu_text" href="">О нас</a></li>
     </ul>
