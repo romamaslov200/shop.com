800e023ed2d96f9110de14b69e8f99b98c7983c5
romamaslov200 <romanmaslov200@gmail.com>
2023-02-28 19:16:22 +0300

    +left header

--- include/functions.php
+++ include/functions.php
@@ -4,11 +4,14 @@ include "connect.php";
 function menu()
 {
     ?>
-    <ul class="menu">
+    <div class="menu">
         <li class="menu_li"><a class="menu_text" href="index">Главная</a></li>
         <li class="menu_li"><a class="menu_text" href="">Поиск</a></li>
-        <li class="menu_li"><a class="menu_text" href="">О нас</a></li>
-    </ul>
+        <div class="menu_rigt">
+            <li class="menu_li"><a class="menu_text" href="">Вход</a></li>
+            <li class="menu_li"><a class="menu_text" href="">Регистрация</a></li>
+        </div>
+    </div>
     <?php
 }
 
